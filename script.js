0e7552fd09006f60f550904d2a440b0f35b86078
Duck-kings <61154137+Duck-kings@users.noreply.github.com>
2020-06-03 14:41:48 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+$(function(){
+    $('.main-menu').mouseenter(function(){
+       /* $('.drop-menu').css('display', 'block');
+
+    });
+    $('.main-menu').mouseleave(function(){
+        $('.drop-menu').css('display', 'none');
+    });*/
+});
